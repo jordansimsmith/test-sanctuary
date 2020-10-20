a651b67425235be6e848e29b97dfc1926f1e4c96
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-10-21 09:30:28 +1300

    Configure prettier for client app

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
