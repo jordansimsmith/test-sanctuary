7fa52618fa44908f1dd378aa1d1eafc0641151c9
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-10-21 16:54:22 +1300

    Integrate apollo client with next

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,22 +1,25 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import withApollo from 'next-with-apollo';
+import App from 'next/app';
 import '../styles/globals.css';
 
-interface MyAppProps {
-  Component: any;
-  pageProps: any;
-}
-
-const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
-});
-
-const MyApp: React.FC<MyAppProps> = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps, apollo }) => {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />;
+    <ApolloProvider client={apollo}>
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 };
 
-export default MyApp;
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
+
+export default withApollo(
+  ({ initialState }) =>
+    new ApolloClient({
+      uri: 'http://localhost:5000/graphql',
+      cache: new InMemoryCache().restore(initialState || {}),
+    }),
+)(MyApp);
