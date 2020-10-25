1663e86eb946b85463d72e4dd6eb4d780568f199
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-10-25 16:50:08 +1300

    Add back button to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { Button, PageHeader } from 'antd';
+import { useRouter } from 'next/dist/client/router';
 import { InstitutionSelector } from './InstitutionSelector';
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
+  const router = useRouter();
+
   return (
     <PageHeader
       className="header"
       title="Test Sanctuary"
       extra={[<InstitutionSelector key="1" />, <Button key="2">Log In</Button>]}
+      onBack={() => router.back()}
     />
   );
 };
